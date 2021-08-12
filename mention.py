34e61786e8acdf48333d3fa3656ccd2ffa3f9642
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-08-12 11:21:21 +0200

    Determine pronominal mention recursively and add method to determine head of mention

--- mention.py
+++ mention.py
@@ -3,12 +3,15 @@
 Class that represents a mention aka a referential expression.
 A mention is definded by a unique id consisting of (sentence_index, start, end)
 """
+import re
+
 from nltk.tree import Tree
 
 
 class Mention:
 
     PRONOUNS = {"PRP", "DT", "PRP$"}
+    NOMINAL = re.compile(r"NN?P?S?")
 
     def __init__(self, mention_id, tree, words):
         self.id = mention_id
@@ -16,24 +19,40 @@ class Mention:
         self.tree = tree
         self.words = words
 
-    def pronominal(self):
-        if len(self.tree) == 1:
-            child = self.tree[0]
-            # Accounts for cases like: (NP (PRP he))
-            if isinstance(child, Tree) and child.label() in self.PRONOUNS:
-                return True
+    def pronominal(self, tree=None):
+        if tree is None:
+            tree = self.tree
+        if len(tree) == 1:
+            child = tree[0]
             # Accounts for cases like: (PRP$ his)
-            elif self.tree.label() in self.PRONOUNS:
+            if tree.label() in self.PRONOUNS:
                 return True
+            # Accounts for cases like: (NP (PRP he))
+            elif isinstance(child, Tree):
+                # This is recursive because pronouns can have structure:
+                # (NP (NP (PRP it)))
+                return self.pronominal(child)
         return False
 
     # TODO: Determine if a mention is indefinite or not.
     def indefinite(self):
         pass
 
-    # TODO: Determine the head noun of a mention.
-    def head(self):
-        pass
+    def head(self, tree=None):
+        if tree is None:
+            tree = self.tree
+        # Base case: tree is pre-terminal.
+        if len(tree) == 1 and not isinstance(tree[0], Tree):
+            return tree[0].split("_")[0]
+        # Because of right headedness, we search for the first
+        # nominal constituent from right to left in all children.
+        for i in range(len(tree)-1, -1, -1):
+            child = tree[i]
+            if isinstance(child, Tree) and self.NOMINAL.search(child.label()):
+                return self.head(child)
+        # This is a fall back: As english has right headedness in nouns,
+        # the naive approach is to assume the last child contains the head.
+        return self.head(tree[-1])
 
     def span(self):
         return self.id[1], self.id[2]
