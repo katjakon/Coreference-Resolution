b202f309aa5132c683a348df77dbbe5ebc2b166a
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-08-15 13:14:29 +0200

    Implement Modifier features

---
+++ compatible_modifiers_only.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 15 11:34:09 2021
+
+@author: HP I5
+"""
+import re
+
+from abstract_feature import AbstractMentionFeature
+
+
+class CompatibleModifiersOnly(AbstractMentionFeature):
+
+    def __init__(self, modifiers=(r"JJ[R|S]?", r"NNP?S?")):
+        self.modifiers = modifiers
+
+    def has_feature(self, antecedent, mention):
+        mod_ant = self._extract_modifiers(antecedent)
+        mod_ment = self._extract_modifiers(mention)
+        if mod_ment.issubset(mod_ant):
+            return True
+        return False
+
+    def _extract_modifiers(self, mention):
+        pos = mention.tree.pos()
+        head = mention.head()
+        modifiers = set()
+        for token, pos in pos:
+            token = token.split("_")[0]
+            for mod in self.modifiers:
+                if re.match(mod, pos) and token != head:
+                    modifiers.add(token)
+        return modifiers
