989a41ad450186c8acc2a43488c2d7b1ab2a23b3
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-08-19 12:23:05 +0200

    Only match acronyms on proper nouns

--- features/acronym.py
+++ features/acronym.py
@@ -6,12 +6,21 @@ A feature that determines if two mentions are acronyms of each other.
 
 class Acronyms:
 
+    def __init__(self, tags=("NNP",)):
+        self.tags = tags
+
     def has_feature(self, mention1, mention2):
         words1 = mention1.words
         words2 = mention2.words
-        if self._upper_letters(words1) == self._upper_letters(words2):
-            return True
-        return False
+        if self._is_proper(mention1) and self._is_proper(mention2):
+            upper1 = self._upper_letters(words1)
+            upper2 = self._upper_letters(words2)
+            if words1 == upper2 or words2 == upper1:
+                return True
+            return False
+
+    def _is_proper(self, mention):
+        return all(pos in self.tags for _, pos in mention.tree.pos())
 
     def _upper_letters(self, words):
         upper = []
@@ -19,4 +28,4 @@ class Acronyms:
             for letter in word:
                 if letter.isupper():
                     upper.append(letter)
-        return upper
+        return ["".join(upper)]
