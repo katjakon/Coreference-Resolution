43fc0014094fa963a025b3e6d3d66d0cc9c3bdc9
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-08-19 20:35:16 +0200

    Implement detection of predicate nominative structures

--- features/predicate_nominative.py
+++ features/predicate_nominative.py
@@ -3,6 +3,7 @@
 A feature that determines if an antecedent and a mention
 are in an predicate nominative construction
 """
+from nltk.tree import Tree
 
 
 class PredicateNominative:
@@ -26,8 +27,30 @@ class PredicateNominative:
                 # Making sure the VP that contains the mention,
                 # is the VP of the S/SBAR tree of the antecedent.
                 if ment_parent.parent() is ant_parent:
-                    # TODO: Make sure VP is predicate
-                    pass
+                    verb = self._get_verb(ment_parent)
+                    if verb:
+                        # Leaves are tuples of token and index.
+                        _, index = verb
+                        # Check if extracted verb is a predicate.
+                        lemma = sentence.lemma()
+                        if lemma[int(index)] in self.predicate:
+                            return True
+        return False
+
+    def _get_verb(self, vp):
+        # Base case: reached a leaf.
+        if not isinstance(vp, Tree):
+            return vp
+        n_verbs = 0
+        index_verb = 0
+        for idx, child in enumerate(vp):
+            # There should be only one verb in the VP.
+            if n_verbs > 1:
+                return None
+            if isinstance(child, Tree) and child.label().startswith("V"):
+                index_verb = idx
+                n_verbs += 1
+        return self._get_predicate(vp[index_verb])
 
     def _get_parent(self, mention, parent_label):
         parent = mention.tree.parent()
