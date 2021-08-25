a079046e8a39fd3bd85c825e68771fc42fbe0b3f
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-08-26 00:03:20 +0200

    Add documentation and comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Main file.
+This file should be executed to extract coreference informations.
 """
 import argparse
 import configparser
@@ -30,10 +31,23 @@ SIEVE_DICT = {"exact_match_sieve": ExactMatchSieve,
               "strict_head_relax_inclusion": StrictHeadRelaxInclusion}
 
 logging.basicConfig(filename=os.path.join(ROOT, "main.log"),
-                    level=logging.DEBUG)
+                    level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s:%(message)s')
 
 
 def find_language_dir(path, language="english"):
+    """Finds directory of the specified language and returns it.
+
+    Args:
+        path:
+            A string or path-like object from which should be looked
+            for the directory.
+        language (str): Name of directory that should be looked for.
+
+    Returns: path to language directory.
+
+    Raises: OSError if directory couldn't be found.
+    """
     for root, dirs, files in os.walk(path):
         if language in dirs:
             lang_dir = os.path.join(root, language)
@@ -44,7 +58,17 @@ def find_language_dir(path, language="english"):
     return lang_dir
 
 
-def extract_files(path, ext="gold_conll"):
+def extract_files(path, ext):
+    """From specified path on extract all
+    files that end with given extension recursively.
+
+    Args:
+        path (str): The path from which should be searched.
+        ext (str): The file extension to extract.
+
+    Returns:
+        List of strings representing file paths.
+    """
     file_paths = []
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -56,6 +80,20 @@ def extract_files(path, ext="gold_conll"):
 
 
 def get_sieves(config_file):
+    """Extract sieves from a config file.
+
+    Config file should have section 'Sieves' under
+    which sieves are specified. Sieves are sorted according to their values.
+
+    Args:
+        config_file (str): A path to a config_file
+
+    Returns:
+        list of sieve instances.
+
+    Raises:
+        OSError if config file has no section 'Sieves.'
+    """
     sieve_key = "Sieves"
     sieves = []
     config = configparser.ConfigParser()
@@ -69,7 +107,13 @@ def get_sieves(config_file):
             logging.warning(f"Unknown sieve '{key}' in config file.")
             continue
         else:
-            position = config["Sieves"].getint(key)
+            try:
+                position = config["Sieves"].getint(key)
+            except ValueError:
+                logging.warning("Invalid literal for int(). "
+                                "Order of sieves might differ.")
+                position = len(sieves)
+            # -1 tells us to ingore a sieve.
             if position == -1:
                 continue
             sieves.append((sieve(), position))
@@ -78,8 +122,20 @@ def get_sieves(config_file):
 
 
 def write_eval_summary(path, eval_list):
+    """Writes a csv file that contains evaluation for each document.
+
+    Args:
+        path (str): A path where file should be written to.
+        eval_list (list):
+            A list of 4-tuples with
+            (filename, precision value, recall value, f1 value)
+
+    Returns: None
+    """
+    # No extracted values found.
     if not eval_list:
         return
+    # Compute averages.
     sum_doc = len(eval_list)
     avg_recall = sum(recall for _, _, recall, _ in eval_list)/sum_doc
     avg_precision = sum(prec for _, prec, _, _ in eval_list)/sum_doc
@@ -88,12 +144,24 @@ def write_eval_summary(path, eval_list):
         csv_writer = csv.writer(file, delimiter=";")
         csv_writer.writerow(["file", "precision", "recall", "f1-score"])
         csv_writer.writerow(["average", avg_precision, avg_recall, avg_f1])
+        # Write values for each document to file.
         for filename, prec, rec, f1 in eval_list:
             csv_writer.writerow([filename, prec, rec, f1])
     logging.info(f"Summary file written to {path}")
 
 
 def coreference_resolution(file, sieves, path_out):
+    """Applies sieves to a document, writes results to file
+    and returns evaluation metrics.
+
+    Args:
+        file (str): Path to file from Ontonotes-Corpus.
+        sieves (list): List of sieve instances.
+        path_out (str): Path to write output file to.
+
+    Returns:
+        4-tuple (filename (str), precision (float), recall (float), f1 (float))
+    """
     doc = Document(file)
     coref = MultiPassResolution(doc, sieves)
     coref.resolve()
@@ -107,22 +175,29 @@ def main():
     parser = argparse.ArgumentParser(description="Coreference Resolution")
     parser.add_argument("in_dir", help="Input directory with conll files.")
     parser.add_argument("out_dir", help="Name of output directory.")
-    parser.add_argument("--config", nargs="?", default="config.txt")
-    parser.add_argument("--ext", nargs="?", default="conll")
-    parser.add_argument("--lang", nargs="?", default=None)
+    parser.add_argument("--config", nargs="?", default="config.txt",
+                        help="Path to config file. Default is 'config.txt'")
+    parser.add_argument("--ext", nargs="?", default="conll",
+                        help=("File extensions that should be considered. "
+                              "Default is 'conll'"))
+    parser.add_argument("--lang", nargs="?", default=None,
+                        help="A dir in in_dir from which files should be extracted.")
     args = parser.parse_args()
-    path = os.path.join(ROOT, args.in_dir)
-    if args.lang:
-        path = find_language_dir(path, args.lang)
-    files = extract_files(path, args.ext)
-    config_file = os.path.join(ROOT, args.config)
-    sieves = get_sieves(config_file)
+    # Get full paths
+    path_in = os.path.join(ROOT, args.in_dir)
     path_out = os.path.join(ROOT, args.out_dir)
+    if args.lang:
+        path_in = find_language_dir(path_in, args.lang)
+    files = extract_files(path_in, args.ext)
+    sieves = get_sieves(os.path.join(ROOT, args.config))
+    # Avoid overwritting existing directories
     if os.path.isdir(path_out):
         raise OSError(f"Output directory {args.out_dir} already exists")
     os.mkdir(path_out)
     eval_list = []
+    # Each document is independent so we can parallelize this process.
     with multiprocessing.Pool() as pool:
+        # Specify arguments for coreference function
         data = [(file, sieves, path_out) for file in files]
         eval_list.extend(pool.starmap(coreference_resolution, data))
     print(f"Output files written to {path_out}")
